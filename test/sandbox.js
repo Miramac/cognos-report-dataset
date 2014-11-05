48db4d19d8c4fe13e0d9962f1e57d0ef631132ad
Miramac <miramac@gmx.net>
2014-11-05 12:23:44 +0100

    project update

--- test/sandbox.js
+++ test/sandbox.js
@@ -13,7 +13,7 @@ fs.readFile(__dirname + '/data/report.xml', function(err, data) {
     parser.parseString(data, function (err, result) {
   //      console.log(util.inspect(result.dataset.metadata[0],{ showHidden: true, depth: null }));
         var report =  new DataSet (result.dataset, { sortBy: ['Split_SortOrder', 'Wave_SortOrder']});
-        console.log(util.inspect(report.data));
+        console.log(util.inspect(report.metadata, {depth:3}));
     });
 });
 
--- test/test.mocha.dataset.js
+++ test/test.mocha.dataset.js
@@ -1,8 +1,8 @@
 /*jshint curly: false, eqeqeq: true, laxcomma: true, node: true, indent: 4*/
+/*global describe, it*/
 var fs = require('fs')
 , xml2js = require('xml2js')
 , assert = require('assert')
-, util = require('util')
 , DataSet = require('../lib/dataset')
 , xmlDataFile = __dirname + '/data/report.xml'
 ;
@@ -19,6 +19,7 @@ describe('Dataset', function(){
                 done();
             });
         });
+        
         it('should have 18 columns', function(done){
             parseXmlreport(function(err, result){
                 if (err) throw err;
@@ -27,6 +28,7 @@ describe('Dataset', function(){
             });
         });
 
+        
         it('should have Report_ID as first column', function(done){
             parseXmlreport(function(err, result){
                 if (err) throw err;
@@ -34,37 +36,48 @@ describe('Dataset', function(){
                    name: 'Report_ID',
                    type: 'xs:int',
                    precision: '1',
-                   isNumber: true
+                   numeric: true
+                });
+                done();
+            });
+        });
+        
+        it('should find Report_ID column', function(done){
+            parseXmlreport(function(err, result){
+                if (err) throw err;
+                assert.deepEqual(result.metadata.getColumn('Report_ID'), {
+                   name: 'Report_ID',
+                   type: 'xs:int',
+                   precision: '1',
+                   numeric: true
                 });
                 done();
             });
         });
 
-        it('should have this data', function(done){
+        it('should have this columns data', function(done){
             parseXmlreport(function(err, result){
                 if (err) throw err;
-                assert.deepEqual(result.metadata, {
-                    columns:
-                       [
-                            { name: 'Report_ID', type: 'xs:int', precision: '1', isNumber: true },
-                            { name: 'ReportType05_ID', type: 'xs:short', precision: '1', isNumber: true },
-                            { name: 'Split_ID', type: 'xs:int', precision: '1', isNumber: true },
-                            { name: 'Split_Label', type: 'xs:string', length: '514', isNumber: false },
-                            { name: 'Split_SortOrder', type: 'xs:string', length: '22', isNumber: false },
-                            { name: 'Wave_ID', type: 'xs:int', precision: '1', isNumber: true },
-                            { name: 'Wave_SortOrder', type: 'xs:string', length: '22', isNumber: false },
-                            { name: 'Wave_LabelShort', type: 'xs:string', length: '512', isNumber: false },
-                            { name: 'N', type: 'xs:int', precision: '1', isNumber: true },
-                            { name: 'Result_Count_Weighted', type: 'xs:double', precision: '2', isNumber: true },
-                            { name: 'Result_Value_Weighted', type: 'xs:double', precision: '2', isNumber: true },
-                            { name: 'SplitResultState_Hide_Result', type: 'xs:int', precision: '1', isNumber: true },
-                            { name: 'delete-slide', type: 'xs:string', length: '40', isNumber: false },
-                            { name: 'Result_Value_Weighted_Max', type: 'xs:double', precision: '2', isNumber: true },
-                            { name: 'Result_Value_Weighted_Min', type: 'xs:double', precision: '2', isNumber: true },
-                            { name: 'VarText_Label01', type: 'xs:string', length: '512', isNumber: false },
-                            { name: 'VarText_EmptyLabel', type: 'xs:string', length: '26', isNumber: false },
-                            { name: 'Footer', type: 'xs:string', length: '512', isNumber: false }
-                       ]});
+                assert.deepEqual(result.metadata.columns, [
+                            { name: 'Report_ID', type: 'xs:int', precision: '1', numeric: true },
+                            { name: 'ReportType05_ID', type: 'xs:short', precision: '1', numeric: true },
+                            { name: 'Split_ID', type: 'xs:int', precision: '1', numeric: true },
+                            { name: 'Split_Label', type: 'xs:string', length: '514', numeric: false },
+                            { name: 'Split_SortOrder', type: 'xs:string', length: '22', numeric: false },
+                            { name: 'Wave_ID', type: 'xs:int', precision: '1', numeric: true },
+                            { name: 'Wave_SortOrder', type: 'xs:string', length: '22', numeric: false },
+                            { name: 'Wave_LabelShort', type: 'xs:string', length: '512', numeric: false },
+                            { name: 'N', type: 'xs:int', precision: '1', numeric: true },
+                            { name: 'Result_Count_Weighted', type: 'xs:double', precision: '2', numeric: true },
+                            { name: 'Result_Value_Weighted', type: 'xs:double', precision: '2', numeric: true },
+                            { name: 'SplitResultState_Hide_Result', type: 'xs:int', precision: '1', numeric: true },
+                            { name: 'delete-slide', type: 'xs:string', length: '40', numeric: false },
+                            { name: 'Result_Value_Weighted_Max', type: 'xs:double', precision: '2', numeric: true },
+                            { name: 'Result_Value_Weighted_Min', type: 'xs:double', precision: '2', numeric: true },
+                            { name: 'VarText_Label01', type: 'xs:string', length: '512', numeric: false },
+                            { name: 'VarText_EmptyLabel', type: 'xs:string', length: '26', numeric: false },
+                            { name: 'Footer', type: 'xs:string', length: '512', numeric: false }
+                       ]);
                 done();
             });
         });
@@ -90,7 +103,7 @@ describe('Dataset', function(){
         it('should have Result_Value_Weighted_Max = 75.98941368', function(done){
             parseXmlreport(function(err, result){
                 if (err) throw err;
-                assert.strictEqual(result.data.rows[0].Split_SortOrder, 75.98941368);
+                assert.strictEqual(result.data.rows[0].Result_Value_Weighted_Max, 75.98941368);
                 done();
             });
         });
